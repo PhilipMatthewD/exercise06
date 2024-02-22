79bc753ac6f3313120badf87052d78c7b60c82a8
SloppyJalopy <matthewprof106@gmail.com>
2024-02-22 13:38:04 -0500

    rofl

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,22 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner input = new Scanner(System.in);
-
     public static void main(String[] args) {
-
+        promptString();
+        promptInt();
+        promptDouble();
+    }
+    static void promptString(){
+        System.out.println("Enter a String:");
+        String userInput = input.nextLine();
+        try {
+            Double.parseDouble(userInput);
+            System.out.println("This is not a String");
+        } catch (NumberFormatException e) {
+            System.out.println(userInput);
+        }
+    }
+    static void promptInt(){
         System.out.println("Enter a Int:");
         try {
             int userInputInt = input.nextInt();
@@ -13,6 +26,9 @@ public class Main {
         } catch (InputMismatchException e) {
             System.out.println("That is not an Integer");
         }
+        input.nextLine();
+    }
+    static void promptDouble(){
         System.out.println("Enter a Double:");
         try {
             input.nextDouble();
@@ -22,7 +38,5 @@ public class Main {
             System.out.println("That is not a Double");
         }
     }
-    static void detectString(){
 
-    }
 }
